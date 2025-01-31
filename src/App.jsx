e86b69686f92a517768b392dd1d39da857a8b312
sunsunarto <sunaryo24259@outlook.com>
2025-01-31 08:16:44 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import { useState } from 'react'
+import Header from './Component/header'
+import './App.css'
+
+function App() {
+
+  return (
+    <div className="App">
+      <Header/>
+    </div>
+  )
+}
+
+export default App
---
+++ src/Component/header.jsx
@@ -0,0 +1,20 @@
+import React from "react"; 
+import '../ComponentStyle/header.css'
+function Header (){
+    return(
+        <div className="header">
+            <div className="leftWord">
+                <h6>Sunsun<span className="headerTitle">arto</span></h6>
+            </div>
+            <div className="navigation">
+                <ul className="navList">
+                    <li className="home"> Home </li>
+                    <li className="about"> About </li>
+                    <li className="contact"> contact </li>
+                </ul>
+            </div>
+        </div>
+    )
+}
+
+export default Header 
